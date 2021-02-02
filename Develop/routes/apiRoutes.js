229b80b5e82984371347ecd22282fa428bcd1cea
Daniel <daniel.castelline@gmail.com>
2021-02-01 19:53:37 -0600

    changed apiroutes, set writefilesync to db.json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,26 +1,25 @@
 const fs = require('fs');
-const express = require('express');
-const uniqid = require('uniqid');
+var data = JSON.parse(fs.readFileSync("./db/db.json", "utf-8"));
 
 module.exports = (app) => {
   // GET
   app.get('/api/notes', (req, res) => res.json(data));
+  app.get('/api/notes/:id', (req, res) => {
+    res.json(data);
+  })
 
   // POST
   app.post('/api/notes', (req, res) => {
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    } else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+    let newNote = req.body;
+    let noteId = (data.length).toString();
+    console.log(noteId);
+    data.push(newNote);
+  
+    fs.writeFileSync('./db/db.json', JSON.stringify(data), (err) => {
+      if (err) throw (err);
+    });
+
+    res.json(data);
   });
 
-  app.post('/api/clear', (req, res) => {
-    tableData.length = 0;
-    waitListData.length = 0;
-
-    res.json({ ok: true });
-  });
 };
